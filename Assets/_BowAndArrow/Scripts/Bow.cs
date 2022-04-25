37d8cb2999807d4ae8330428fa0ae704af317693
Ownerkubus <kubusdenouden@gmail.com>
2022-04-25 12:42:18 +0200

    bow fix

--- Assets/_BowAndArrow/Scripts/Bow.cs
+++ Assets/_BowAndArrow/Scripts/Bow.cs
@@ -1,7 +1,13 @@
-﻿using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Bow : XRGrabInteractable
 {
+    private Vector3 interactorPosition = Vector3.zero;
+    private Quaternion interactorRotation = Quaternion.identity;
+
     private Notch notch = null;
 
     protected override void Awake()
@@ -24,5 +30,50 @@ public class Bow : XRGrabInteractable
         base.OnDisable();
         selectEntered.RemoveListener(notch.SetReady);
         selectExited.RemoveListener(notch.SetReady);
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        if (args.interactor.GetType() != typeof(XRSocketInteractorTag))
+        {
+            StoreInteractor(args.interactor);
+            MatchAttachmentPoints(args.interactor);
+        }
+    }
+
+    private void StoreInteractor(XRBaseInteractor interactor)
+    {
+        interactorPosition = interactor.attachTransform.localPosition;
+        interactorRotation = interactor.attachTransform.localRotation;
+    }
+
+    private void MatchAttachmentPoints(XRBaseInteractor interactor)
+    {
+        bool hasAttach = attachTransform != null;
+        interactor.attachTransform.position = hasAttach ? attachTransform.position : transform.position;
+        interactor.attachTransform.rotation = hasAttach ? attachTransform.rotation : transform.rotation;
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        if (args.interactor.GetType() != typeof(XRSocketInteractorTag))
+        {
+            ResetAttachmentPoints(args.interactor);
+            ClearInteractor(args.interactor);
+        }
+    }
+
+    private void ResetAttachmentPoints(XRBaseInteractor interactor)
+    {
+        interactor.attachTransform.localPosition = interactorPosition;
+        interactor.attachTransform.localRotation = interactorRotation;
+    }
+
+    private void ClearInteractor(XRBaseInteractor interactor)
+    {
+        interactorPosition = Vector3.zero;
+        interactorRotation = Quaternion.identity;
     }
 }
