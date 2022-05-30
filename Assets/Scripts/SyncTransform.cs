4fc01f33ae8e9fbd762c2d45da846dbf3b830fa1
Tom Renkema <tomrenkema@live.nl>
2022-05-31 00:40:19 +0200

    Update

---
+++ Assets/Scripts/SyncTransform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SyncTransform : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    private void Update()
+    {
+        if (!PV.IsMine)
+            return;
+
+        PV.RPC("RPC_SyncTransform", RpcTarget.Others, transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    [PunRPC]
+    public void RPC_SyncTransform(float _xPos, float _yPos, float _zPos)
+    {
+        transform.position = new Vector3(_xPos, _yPos, _zPos);
+    }
+}
