fef8ad87453cb8d3bca7e96b02b69b051fda29ce
Tom Renkema <tomrenkema@live.nl>
2022-05-31 00:46:58 +0200

    Update

--- Assets/Scripts/SyncTransform.cs
+++ Assets/Scripts/SyncTransform.cs
@@ -18,11 +18,15 @@ public class SyncTransform : MonoBehaviour
             return;
 
         PV.RPC("RPC_SyncTransform", RpcTarget.Others, transform.position.x, transform.position.y, transform.position.z);
+
+        Debug.Log("Pos: " + transform.position);
     }
 
     [PunRPC]
     public void RPC_SyncTransform(float _xPos, float _yPos, float _zPos)
     {
         transform.position = new Vector3(_xPos, _yPos, _zPos);
+
+        Debug.Log("Pos Y: " + _yPos);
     }
 }
