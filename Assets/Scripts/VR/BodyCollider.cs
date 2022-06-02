2abd211df1b071e3af89bd9cdac4af4c35d384e6
Tom Renkema <tomrenkema@live.nl>
2022-06-02 16:21:43 +0200

    Update

---
+++ Assets/Scripts/VR/BodyCollider.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR.CoreUtils;
+
+public class BodyCollider : MonoBehaviour
+{
+    [SerializeField] XROrigin xrRig;
+
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] LayerMask groundLayer;
+
+    [SerializeField] float gravity = -9.81f;
+
+    float fallingSpeed;
+
+    private void FixedUpdate()
+    {
+        if (xrRig.CameraInOriginSpacePos != null)
+        {
+            var center = xrRig.CameraInOriginSpacePos;
+
+            controller.center = new Vector3(center.x, controller.center.y, center.z);
+
+            controller.height = xrRig.CameraInOriginSpaceHeight;
+
+            bool isGrounded = CheckIfGrounded();
+
+            if (isGrounded)
+                fallingSpeed = 0;
+            else
+            {
+                fallingSpeed += gravity * Time.fixedDeltaTime;
+            }
+
+            controller.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    bool CheckIfGrounded()
+    {
+        Vector3 rayStart = transform.TransformPoint(controller.center);
+        float rayLength = controller.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, controller.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
+    }
+}
