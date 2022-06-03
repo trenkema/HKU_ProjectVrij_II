c6f29f52e7f1d433f499bf360bee00a0340e50d0
Tom Renkema <tomrenkema@live.nl>
2022-06-03 19:23:15 +0200

    Update

--- Assets/Scripts/VR/BodyCollider.cs
+++ Assets/Scripts/VR/BodyCollider.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 using Unity.XR.CoreUtils;
+using Photon.Pun;
 
-public class BodyCollider : MonoBehaviour
+public class BodyCollider : MonoBehaviourPun, IPunObservable
 {
     [Header("References")]
+    [SerializeField] PhotonView PV;
+
     [SerializeField] XROrigin xrRig;
 
     [SerializeField] CharacterController controller;
@@ -16,6 +19,9 @@ public class BodyCollider : MonoBehaviour
 
     private void Update()
     {
+        if (!PV.IsMine)
+            return;
+
         if (xrRig.CameraInOriginSpacePos != null)
         {
             controller.center = new Vector3(target.localPosition.x, controller.height / 2 + controller.skinWidth, target.localPosition.z);
@@ -24,6 +30,20 @@ public class BodyCollider : MonoBehaviour
 
             controller.Move(new Vector3(0.001f, -0.001f, 0.001f));
             controller.Move(new Vector3(-0.001f, -0.001f, -0.001f));
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(controller.center);
+            stream.SendNext(controller.height);
+        }
+        else if (stream.IsReading)
+        {
+            controller.center = (Vector3)stream.ReceiveNext();
+            controller.height = (float)stream.ReceiveNext();
         }
     }
 }
