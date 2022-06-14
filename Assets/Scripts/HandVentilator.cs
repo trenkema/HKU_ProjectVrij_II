76cafa6d5dc3a23d3e4b30b3fe75bd3091a68693
Ownerkubus <kubusdenouden@gmail.com>
2022-06-14 17:20:45 +0200

    Draaiende wieken

--- Assets/Scripts/HandVentilator.cs
+++ Assets/Scripts/HandVentilator.cs
@@ -6,6 +6,14 @@ public class HandVentilator : MonoBehaviour
 {
 
     [SerializeField] float forwardForce = 1f;
+    [SerializeField] float rotationSpeed = 100f;
+
+    [SerializeField] GameObject blades;
+
+    private void Update()
+    {
+        blades.transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
 
     void OnTriggerEnter(Collider other)
     {
