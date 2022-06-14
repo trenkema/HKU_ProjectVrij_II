66e38860badfc4fee74aa1f156ee3ef826460f57
Ownerkubus <kubusdenouden@gmail.com>
2022-06-14 17:03:25 +0200

    Hand Ventilator werkend

--- Assets/Scripts/HandVentilator.cs
+++ Assets/Scripts/HandVentilator.cs
@@ -7,21 +7,9 @@ public class HandVentilator : MonoBehaviour
 
     [SerializeField] float forwardForce = 1f;
 
-    [SerializeField] public Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider other)
     {
-        rb.AddForce(transform.forward * forwardForce);
+        other.attachedRigidbody.AddForce(transform.forward * forwardForce);
+        //Debug.Log(other.attachedRigidbody);
     }
 }
